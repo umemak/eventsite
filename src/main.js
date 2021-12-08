4a739a35ff5bc3cf5f2237047d6149e33f428235
umemak <umematsu@gmail.com>
2021-12-08 14:45:03 +0000

    add views

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import {
   applyPolyfills,
   defineCustomElements,
 } from '@aws-amplify/ui-components/loader';
+import router from './router'
 
 Amplify.configure(aws_exports);
 applyPolyfills().then(() => {
---
+++ src/views/Top.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>トップ</h1>
+  </div>
+  <p>最近のイベント</p>
+  <button>ログイン</button>
+  <button>検索</button>
+</template>
+
+<script>
+export default {
+  name: "Top",
+};
+</script>
