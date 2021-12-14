cfb804b396abf5f7ca1184b88d5193f7517e00dc
umemak <umematsu@gmail.com>
2021-12-14 14:30:55 +0000

    add create, list

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,19 +2,39 @@
     <div class="create">
         <h1>イベント作成</h1>
         <ul>
-            <li>タイトル: <input v-model="title" /></li>
+            <li>タイトル: <input v-model="name" /></li>
             <li>開催日時: <input v-model="date" /></li>
             <li>開催場所: <input v-model="place" /></li>
             <li>公開期間: <input v-model="open" />～<input v-model="close" /></li>
         </ul>
-        <button @click="eventCreate">作成</button>
+        <button @click="createEvent">作成</button>
     </div>
 </template>
 
 <script>
+import { API } from 'aws-amplify';
+import { createEvent } from '../graphql/mutations';
+
 export default {
+  name: 'EventCreate',
+  data(){
+    return {
+      name: '',
+      date: '',
+      place: '',
+      open: '',
+      close: ''
+    }
+  },
   methods: {
-    eventCreate: function() {
+    async createEvent() {
+      const { name, date, place, open, close } = this;
+      if (!name || !date) return;
+      const event = { name, date, place, open, close };
+      await API.graphql({
+        query: createEvent,
+        variables: {input: event},
+      });
 
       this.$router.push('/event')
     },
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,22 +1,43 @@
 <template>
-    <div class="top">
-        <h1>トップ</h1>
-        <button @click="login">ログイン</button><br>
-        <button @click="search">検索</button>
+  <div class="top">
+    <h1>トップ</h1>
+    <button @click="login">ログイン</button><br />
+    <button @click="search">検索</button>
+
+    <div v-for="item in events" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.place }}</p>
     </div>
+  </div>
 </template>
 
 <script>
+import { API } from "aws-amplify";
+import { listEvents } from "../graphql/queries";
+
 export default {
+  name: "Top",
+  async created() {
+    this.getEvents();
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
   methods: {
-    login: function() {
-
-      this.$router.push('/login')
+    login: function () {
+      this.$router.push("/login");
     },
-    search: function() {
-
-      this.$router.push('/search')
+    search: function () {
+      this.$router.push("/search");
+    },
+    async getEvents() {
+      const events = await API.graphql({
+        query: listEvents,
+      });
+      this.events = events.data.listEvents.items;
     },
   },
-}
+};
 </script>
