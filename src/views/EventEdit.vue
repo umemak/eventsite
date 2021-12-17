57f1ee4d7d2edabad07a7589b6e5fa1c99393944
umemak <umematsu@gmail.com>
2021-12-17 12:31:15 +0000

    add update

--- src/views/EventEdit.vue
+++ src/views/EventEdit.vue
@@ -1,23 +1,80 @@
 <template>
-    <div class="edit">
-        <h1>イベント編集</h1>
-        <ul>
-            <li>タイトル: <input v-model="title" /></li>
-            <li>開催日時: <input v-model="date" /></li>
-            <li>開催場所: <input v-model="place" /></li>
-            <li>公開期間: <input v-model="open" />～<input v-model="close" /></li>
-        </ul>
-        <button @click="eventUpdate">保存</button>
-    </div>
+  <div class="edit">
+    <h1>イベント編集</h1>
+    <ul>
+      <li>タイトル: <input v-model="event.name" /></li>
+      <li>開催日時: <input v-model="event.date" /></li>
+      <li>開催場所: <input v-model="event.place" /></li>
+      <li>
+        公開期間: <input v-model="event.open" />～<input
+          v-model="event.close"
+        />
+      </li>
+    </ul>
+    <button @click="eventUpdate">保存</button>
+  </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    eventUpdate: function() {
+import { API } from "aws-amplify";
+import { getEvent } from "../graphql/queries";
+import { updateEvent } from "@/graphql/mutations";
 
-      this.$router.push('/event')
+export default {
+  name: "EventEdit",
+  props: {
+    eventId: String,
+  },
+  async created() {
+    this.getEvent();
+  },
+  data() {
+    return {
+      event: {
+        id: "",
+        name: "",
+        date: "",
+        place: "",
+        open: "",
+        close: "",
+      },
+    };
+  },
+  methods: {
+    async getEvent() {
+      await API.graphql({
+        query: getEvent,
+        variables: { id: this.eventId },
+      })
+        .then((result) => {
+          console.log(result);
+          this.event.id = result.data.getEvent.id;
+          this.event.name = result.data.getEvent.name;
+          this.event.date = result.data.getEvent.date;
+          this.event.place = result.data.getEvent.place;
+          this.event.open = result.data.getEvent.open;
+          this.event.close = result.data.getEvent.close;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async eventUpdate() {
+      await API.graphql({
+        query: updateEvent,
+        variables: { input: this.event },
+      })
+        .then((result) => {
+          console.log(result);
+          this.$router.push({
+            name: "Event",
+            params: { eventId: this.event.id },
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
-}
+};
 </script>
--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,35 +1,60 @@
 <template>
-    <div class="mypage">
-        <h1>マイページ</h1>
-        <ul>
-            <li>作成済みイベント:
-                <ul>
-                    <li>イベント１</li><button @click="eventEdit">編集</button>
-                    <li>イベント２</li><button @click="eventEdit">編集</button>
-                </ul>
-            </li>
-            <li>エントリー済みイベント:
-                <ul>
-                    <li>イベント１</li>
-                    <li>イベント２</li>
-                </ul>
-            </li>
-        </ul>
-        <button @click="eventCreate">イベント作成</button>
+  <div class="mypage">
+    <h1>マイページ</h1>
+    <h3>作成済みイベント</h3>
+    <div v-for="item in myEvents" :key="item.id">
+      <router-link
+        custom
+        v-slot="{ navigate }"
+        :to="{ name: 'Event', params: { eventId: item.id } }"
+      >
+        <button @click="navigate">{{ item.name }}@{{ item.place }}</button>
+      </router-link>
+      <router-link
+        custom
+        v-slot="{ navigate }"
+        :to="{ name: 'EventEdit', params: { eventId: item.id } }"
+      >
+        <button @click="navigate">編集</button>
+      </router-link>
     </div>
+    <h3>エントリー済みイベント</h3>
+    <li>
+      <ul>
+        <li>イベント１</li>
+        <li>イベント２</li>
+      </ul>
+    </li>
+    <button @click="eventCreate">イベント作成</button>
+  </div>
 </template>
 
 <script>
+import { API } from "aws-amplify";
+import { listEvents } from "../graphql/queries";
+
 export default {
+  async created() {
+    this.getMyEvents();
+  },
+  data() {
+    return {
+      myEvents: [],
+    };
+  },
   methods: {
-    eventEdit: function() {
-
-      this.$router.push('/edit')
+    async getMyEvents() {
+      const events = await API.graphql({
+        query: listEvents,
+      });
+      this.myEvents = events.data.listEvents.items;
     },
-    eventCreate: function() {
-
-      this.$router.push('/create')
+    eventEdit: function () {
+      this.$router.push("/edit");
+    },
+    eventCreate: function () {
+      this.$router.push("/create");
     },
   },
-}
+};
 </script>
